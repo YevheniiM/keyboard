e569e82df530dbef085420968f9c8bd5b334321b
YevheniM <xmolodtsov@gmail.com>
2020-03-15 20:35:09 +0200

    Add configuration file

--- BE/advanced_layout/testing.py
+++ BE/advanced_layout/testing.py
@@ -8,6 +8,15 @@ def generate_events():
         yield keyboard.read_event()
 
 
-strings = keyboard.get_typed_strings(generate_events())
-while True:
-    print(next(strings))
+# strings = keyboard.get_typed_strings(generate_events())
+# while True:
+#     print(next(strings))
+
+def get_value(char):
+    keyboard.send('backspace')
+    keyboard.write('b')
+
+
+keyboard.remap_hotkey('ctrl+a', 'q')
+# keyboard.add_hotkey('a', get_value, args=['b'])
+keyboard.wait()
